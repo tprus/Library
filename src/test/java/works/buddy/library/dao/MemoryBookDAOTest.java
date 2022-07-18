c0ecf446732685c8f190aadce23dc3a9b75204fb
Adam Lewandowski <adam.lewandowski@buddy.works>
2022-07-18 10:20:40 +0200

    Added 2 tests for save()

--- src/test/java/works/buddy/library/dao/MemoryBookDAOTest.java
+++ src/test/java/works/buddy/library/dao/MemoryBookDAOTest.java
@@ -1,5 +1,6 @@
 package works.buddy.library.dao;
 
+import org.junit.Before;
 import org.junit.Test;
 import works.buddy.library.model.Book;
 
@@ -13,17 +14,34 @@ import static org.testng.AssertJUnit.assertEquals;
 public class MemoryBookDAOTest {
     private static final List<Book> SAMPLE_BOOKS = new ArrayList<>(
             List.of(new Book("1984", "Scott Chacon and Ben Straub"), new Book("Thinking in Java", "Bruce Eckel")));
+
+
     @Test
     public void getBooksTest(){
         MemoryBookDAO tested =  new MemoryBookDAO(SAMPLE_BOOKS);
         assertEquals("Collections should match", SAMPLE_BOOKS, tested.getBooks());
     }
 
-    // ten test na razie ma nie przejść
     @Test
     public void getBooksWithNullConstructorTest(){
         MemoryBookDAO tested =  new MemoryBookDAO(null);
         assertEquals("we shouldn't have a null object as a collection", new ArrayList<Book>(), tested.getBooks());
     }
-    
+
+    @Test
+    public void saveTest(){
+        Book expected = new Book("Jakaś nazwa", "Jakiś autor");
+        MemoryBookDAO tested =  new MemoryBookDAO(new ArrayList<>() );
+        tested.save(expected);
+        Book actual = tested.getBooks().iterator().next();
+        assertEquals(1,tested.getBooks().size());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void saveTestWithNullBook(){
+        MemoryBookDAO tested =  new MemoryBookDAO(new ArrayList<>() );
+        tested.save(null);
+        Book actual = tested.getBooks().iterator().next();
+        assertEquals(0,tested.getBooks().size());
+    }
 }
