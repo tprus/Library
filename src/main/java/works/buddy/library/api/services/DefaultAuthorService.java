158e734e20ed7dc00e0f7269c2a05aab9df39c52
Adam Lewandowski <adam.lewandowski@buddy.works>
2022-08-02 10:05:56 +0200

    validation for createAuthorMethod

--- src/main/java/works/buddy/library/api/services/DefaultAuthorService.java
+++ src/main/java/works/buddy/library/api/services/DefaultAuthorService.java
@@ -3,6 +3,8 @@ package works.buddy.library.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import works.buddy.library.api.ApiConstants;
+import works.buddy.library.api.errors.BadRequestException;
 import works.buddy.library.api.view.AuthorFront;
 import works.buddy.library.api.view.AuthorsFront;
 import works.buddy.library.dao.AuthorDAO;
@@ -30,6 +32,7 @@ public class DefaultAuthorService implements AuthorService {
 
     @Override
     public AuthorFront createAuthor(AuthorFront authorFront) {
+        validate(authorFront);
         Author author = getAuthor(authorFront);
         authorDAO.save(author);
         return getAuthorFront(author);
@@ -49,5 +52,26 @@ public class DefaultAuthorService implements AuthorService {
             mappedAuthors.add(new AuthorFront(author));
         }
         return new AuthorsFront(mappedAuthors);
+    }
+
+    private void validate(AuthorFront author) {
+        if (author.getFirstName() == null) {
+            throw new BadRequestException("Authors first name is required");
+        }
+        if (author.getFirstName().length() < ApiConstants.MIN_NAME_LENGTH) {
+            throw new BadRequestException("Authors first name has to be longer than " + ApiConstants.MIN_NAME_LENGTH + " characters");
+        }
+        if (author.getFirstName().length() > ApiConstants.MAX_NAME_LENGTH) {
+            throw new BadRequestException("Authors first name has to be longer than " + ApiConstants.MAX_NAME_LENGTH + " characters");
+        }
+        if (author.getLastName() == null) {
+            throw new BadRequestException("Authors last name is required");
+        }
+        if (author.getLastName().length() < ApiConstants.MIN_NAME_LENGTH) {
+            throw new BadRequestException("Authors last name has to be longer than " + ApiConstants.MIN_NAME_LENGTH + " characters");
+        }
+        if (author.getLastName().length() > ApiConstants.MAX_NAME_LENGTH) {
+            throw new BadRequestException("Authors last name has to be longer than " + ApiConstants.MAX_NAME_LENGTH + " characters");
+        }
     }
 }
